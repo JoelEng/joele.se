cfd6d9d2588741455ca07d003e47fc36a2453e2a
JoelEngstrom <joel.n.engstrom@protonmail.com>
2022-02-05 00:15:34 +0100

    Social media links

--- components/LayerButton.js
+++ components/LayerButton.js
@@ -1,12 +1,11 @@
 import styles from './../styles/LayerButton.module.sass'
 
-const LayerButton = ({children, ...props}) => (
+const LayerButton = ({children, background, fontFamily, ...props}) => (
   <a href={props.href} download={props.download} className={styles.LayerButton} style={{ borderRadius: props.borderRadius, pointerEvents: props.pointerEvents }} >
-    <div className={styles.LayerButtonBackground} />
-    <div className={styles.LayerButtonForeground} {...props}>
-      <div>
-        {children}
-      </div>
+    <div className={styles.LayerButtonBackground} style={{ background: background }} />
+    <div className={styles.LayerButtonForeground}/>
+    <div className={styles.ButtonChild} {...props} >
+      {children}
     </div>
   </a>
 )
